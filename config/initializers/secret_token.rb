bdf4c8ede98f73f29be7fca50a4c59846ff54427
Adler Santos <adlersantos712@gmail.com>
2013-08-15 11:04:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditClone::Application.config.secret_token = 'c2fdaecda26d670ef041ebce0a5ae6187f19e4d1c86a5249fd8954d669aa4777f55a44f342e1efe5a1b349d009e76f011633c2038133738a0869afd22276ccd6'
